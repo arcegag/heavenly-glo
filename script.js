6868ac933a685a51d9ac7fe83ae5173eb5e11cdb
arcegag <pnoysendit@yahoo.com>
2025-03-12 21:27:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function toggleMenu() {
-    const menu = document.getElementById("mobile-menu");
-    if (menu.style.right === "0px") {
-        menu.style.right = "-250px";
-    } else {
-        menu.style.right = "0px";
-    }
-}
 
-function closeMenu() {
-    document.getElementById("mobile-menu").style.right = "-250px";
-}
