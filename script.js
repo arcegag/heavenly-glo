a492c2a129fc109271d3c9a17fb79e5200deb27e
arcegag <pnoysendit@yahoo.com>
2025-03-12 20:53:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function toggleMenu() {
-    document.getElementById("menu").classList.toggle("show");
-}
-
-function showSection(id) {
-    document.querySelectorAll('.content-section').forEach(section => {
-        section.style.display = "none";
-    });
-    document.getElementById(id).style.display = "block";
+    const menu = document.getElementById("mobile-menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
