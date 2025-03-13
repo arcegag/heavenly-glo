055f67473bc371ad58699afe1dcba5297df3a469
arcegag <pnoysendit@yahoo.com>
2025-03-12 21:33:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// This file is currently not used, but you can expand for interactive functionality in the future.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website Loaded Successfully");
+});
