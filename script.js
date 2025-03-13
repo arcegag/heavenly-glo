9725d918f629f3bf1690206e738b23133eb69c82
arcegag <pnoysendit@yahoo.com>
2025-03-12 21:13:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function toggleMenu() {
     document.getElementById("mobile-menu").classList.toggle("active");
 }
-
-function closeMenu() {
-    document.getElementById("mobile-menu").classList.remove("active");
-}
